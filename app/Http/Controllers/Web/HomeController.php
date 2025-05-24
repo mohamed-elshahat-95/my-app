0f1ba71b857ff2faf0e5a3e19ceb1239fd3958ed
Mohamed Elshahat <70586903+mohamed-elshahat-95@users.noreply.github.com>
2025-05-24 18:38:18 +0300

    fix home page

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     public function index(){
-       return view('crm.home.index');
+    //    return view('crm.home.index');
+       return view('crm.home.me');
     }
 }
